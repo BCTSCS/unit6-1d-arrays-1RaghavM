fb11c69a2e731aa52abff6743ff2614ebd57e11c
1RaghavM <lebrongoated2323@gmail.com>
2025-02-07 14:06:50 -0500

    Do Now 2/7/25

--- DataAnalyzer.java
+++ DataAnalyzer.java
@@ -1,8 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class DataAnalyzer {
-    
+    public static int[] reverseList(int[] numbers){
+        for(int i = 0; i<numbers.length / 2; i++){
+            int temp = numbers[i];
+            numbers[i] = numbers[numbers.length -1 - i];
+            numbers[numbers.length -1 - i] = temp;
+        }
+        return numbers;
+    }
     
     public static int searchList(int[] numbers , int target){
-        
         int index = 0;
         while(index<=numbers.length - 1){
             
@@ -37,8 +47,25 @@ public class DataAnalyzer {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10,20,30,40,50};
-        System.out.println(searchList(arr, 50));
-        System.out.println(binarySearch(50, arr));
+        int[] arr = new int[100];
+        try {
+            File f = new File("numbers.txt");
+            Scanner input = new Scanner(f);
+            for(int i = 0; i<100; i++){
+                arr[i] = input.nextInt();
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("file not found");
+        }
+
+        System.out.println(searchList(arr,32));
+        int []rarr = (reverseList(arr));
+        for(int i = 0; i< rarr.length; i++){
+            System.out.println(rarr[i]+ " ");
+        }
     }
+
+        
 }
